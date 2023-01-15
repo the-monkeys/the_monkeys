d8bfa8ebfb44e3c93927e0271e7c2095be89917f
shoa1bdev <s.akhtar7958@gmail.com>
2023-01-15 22:19:18 +0530

    created navigation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navigation from './Navigation/Navigation';
+import About from './Navigation/About';
+import Contact from './Navigation/Contact';
+import './index.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return(
+  <>
+   <Navigation />
+   <Routes>
+    <Route path='/' element={App}/>
+    <Route path='/about' element={<About />} />
+    <Route path='/contact' element={<Contact />} />
+   </Routes>
+  </>)
+    
+}
+
+export default App;
