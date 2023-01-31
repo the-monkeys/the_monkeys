f97d75e94b3f9d6232ec02f8379bb1c046d34da5
khushaboopandey <pandeykhushaboo@gmail.com>
2023-01-31 22:58:39 +0530

    git commit -m "AdScript changes"

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import AdScript from "./AdScript";
 
 function App() {
   return (
     <>
       <Navigation />
+      {/* AdScript will add script tag to all the component present here */}
+      <AdScript />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/tos" element={<Legal/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/tos" element={<Legal />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
