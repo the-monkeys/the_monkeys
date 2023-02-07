c48581d9fe4e8c5dc3e4681f823f52b187ba9bf9
Shoaib Akhtar <64205616+shoa1bdev@users.noreply.github.com>
2023-02-07 21:25:18 +0530

    login/signup Page Updated. (#30)
    * Made Seperate component for Login/Signup buttons.
    
    * Login Component Changed
    
    * Register Component Changed
    
    * Dropdown Bug Fixed.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,35 +1,29 @@
-import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Logo } from "../Logo";
+import { Dropdown } from "./Dropdown/Dropdown";
+import { SignupBtn } from './SignupBtn'
 
 export const Navigation = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+
+  document.onclick = function(clickevent) {
+    if (clickevent.target.id !== 'menu') {
+      setShowDropdown(false)
+    }
+  }
   return (
     <nav className="relative md:px-2 shadow-sm z-10">
       <div className="container mx-auto">
         <div className="flex items-center justify-between py-4">
           <Logo />
-          <div className="hidden md:flex justify-between items-center space-x-6">
-            <NavLink
-              to="/login"
-              className="py-2 px-6 bg-transparent baseline 
-              cursor-pointer border-2 border-transparent main-link hover:text-black "
-            >
-              Login
-            </NavLink>
-            <NavLink
-              className="py-2 px-6 text-white  bg-lightBlack baseline
-             cursor-pointer rounded-sm hover:bg-transparent hover:text-lightBlack
-             border-2 hover: border-lightBlack"
-              to="/register"
-            >
-              Sign Up
-            </NavLink>
-          </div>
-          {/* Hamburger Menu Button */}
+          <SignupBtn />
           <button
+          id="menu"
             onClick={() => {
-              document.querySelector(".mobile-nav").classList.toggle("hidden");
+              setShowDropdown(!showDropdown);
             }}
-            className="md:hidden h-[20px] w-[25px] flex flex-col justify-between"
+            className="md:hidden h-[20px] w-[25px] flex flex-col justify-between z-1"
           >
             <span className="block h-0.5 w-8 bg-lightBlack"></span>
             <span className="block h-0.5 w-8 bg-lightBlack"></span>
@@ -38,59 +32,29 @@ export const Navigation = () => {
         </div>
       </div>
       <div className="hidden flex container md:flex items-center justify-center">
-        <ul className="py-4 flex justify-center items-center space-x-14">
-          <Link className="navlinks main-link" to="/">
-            Trending
-          </Link>
-          <Link className="navlinks main-link" to="/">
-            Technology
-          </Link>
-          <Link className="navlinks main-link" to="/">
-            Lifestyle
-          </Link>
-          <Link className="navlinks main-link" to="/">
-            Health
-          </Link>
-          <Link className="navlinks main-link" to="/">
-            Business & Finance
-          </Link>
-          <Link className="navlinks main-link" to="/">
-            Philosophy & psychology
-          </Link>
-        </ul>
-      </div>
-      {/* Dropdown Nav */}
-      <div className="md:hidden">
         <ul
-          className="mobile-nav hidden absolute flex flex-col items-center self-end py-8 space-y-6 
-        bg-white sm:self-center w-full shadow-md"
-        >
-          <Link className="" to="/contact">
+         className="py-4 flex justify-center items-center space-x-14">
+          <Link className="navlinks main-link" to="/">
             Trending
           </Link>
-          <Link className="" to="/">
+          <Link className="navlinks main-link" to="/">
             Technology
           </Link>
-          <Link className="" to="/">
+          <Link className="navlinks main-link" to="/">
             Lifestyle
           </Link>
-          <Link className="" to="/">
+          <Link className="navlinks main-link" to="/">
             Health
           </Link>
-          <Link className="" to="/">
+          <Link className="navlinks main-link" to="/">
             Business & Finance
           </Link>
-          <Link className="" to="/">
+          <Link className="navlinks main-link" to="/">
             Philosophy & psychology
-          </Link>
-          <Link className="" to="/register">
-            Register
-          </Link>
-          <Link className="" to="/login">
-            Login
           </Link>
         </ul>
       </div>
+      {showDropdown ? <Dropdown /> : ''}
     </nav>
-  );
-};
+  )
+}
