794c1bfa7b6f0ba386c571f356cd264192bcca64
Shoaib Akhtar <S.akhtar7958@gmail.com>
2023-03-10 18:57:31 +0530

    login status created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home/Home";
 import { Legal } from "./pages/Legal/Legal";
 import "./index.css";
+import { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Auth/components/Login";
 import { Register } from "./pages/Auth/components/Register";
@@ -10,9 +11,10 @@ import AdScript from "./AdScript";
 import "izitoast-react/dist/iziToast.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
     <>
-      <Navigation />
+      <Navigation isLoggedIn={isLoggedIn} />
       {/* AdScript will add script tag to all the component present here */}
       <AdScript />
       <Routes>
