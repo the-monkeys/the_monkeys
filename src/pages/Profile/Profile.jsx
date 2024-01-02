26461b9359ea8aa9a97d9015e48628fdd304f562
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2024-01-02 11:40:58 +0530

    Eliminate unused dependency (#113)
    * all material ui packages removed
    
    * react quill package removed
    
    * headless ui heroicon devtoolextension styledcomponent removed

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from "react";
-import ImgBg from "../../images/profilebg.jpeg";
-import { HiUserCircle } from "react-icons/hi";
-import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
-import { BsPinterest } from "react-icons/bs";
-import {
-  SiAdobeaftereffects,
-  SiAdobeillustrator,
-  SiAdobeindesign,
-  SiAdobephotoshop,
-  SiAdobexd,
-} from "react-icons/si";
 import { useSelector } from "react-redux";
 import UserService from "../../utils/UserService";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { ExitToApp as LogoutIcon } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import "./profile.css";
-import { useHistory } from "react-router-dom";
 
 export const Profile = () => {
   const [loadData, setloadData] = useState([]);
@@ -45,129 +29,97 @@ export const Profile = () => {
   return (
     <div className="rootProfile">
       <div className="header-root">
-        <Typography variant="h5" component="h1" className="headingProfile">
+        <p component="h1" className="headingProfile">
           Hi, Monkey !
-        </Typography>
+        </p>
 
-        <Typography variant="body2" className="greeting">
-          Welcome back! Happy Reading!
-        </Typography>
+        <p className="greeting">Welcome back! Happy Reading!</p>
       </div>
 
       <div className="profileConatiner">
         <div className="leftCotainer">
-          <h4
-          
-            className="profileHeadingLeft"
-          >
-            Profile Overview
-          </h4>
+          <h4 className="profileHeadingLeft">Profile Overview</h4>
           <div className="profileSection">
-            <Avatar
-              alt="Monkey Image"
-              src="../../images/the_monkeys.png"
-              className="profileAvatar"
-            />
             <div className="leftDetails">
-              <Typography className="profileText">
+              <p className="profileText">
                 <h5 className="profileSubHeading">Name :</h5>
                 The monkey
-              </Typography>
-              <Typography className="profileText">
+              </p>
+              <p className="profileText">
                 <h5 className="profileSubHeading">Email : </h5>
-               goyalyash1211@gmail.com
-              </Typography>
-              <Typography className="profileText">
-                <h5 className="profileSubHeading">Member since :</h5>{" "}
-                Dec 25,2023
-              </Typography>
+                goyalyash1211@gmail.com
+              </p>
+              <p className="profileText">
+                <h5 className="profileSubHeading">Member since :</h5> Dec
+                25,2023
+              </p>
             </div>
           </div>
 
           <div className="myOrder">
-            <Typography variant="h4" component="h1" className="profileHeading">
+            <p component="h1" className="profileHeading">
               Blogs
-            </Typography>
+            </p>
             <Link
               to="/orders"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              <Button variant="contained" className="blogsButton">
-                Blogs
-              </Button>
+              <button className="blogsbutton">Blogs</button>
             </Link>
           </div>
         </div>
 
         <div className="rightConatiner">
           <div className="righHeadings">
-            <Typography variant="h4" component="h1" className="profileHeading">
+            <p component="h1" className="profileHeading">
               Personal Information
-            </Typography>
-            <Typography className="profileText2">
+            </p>
+            <p className="profileText2">
               Hey there ! Feel free to edit any of your details below so your
               account is up to date.
-            </Typography>
+            </p>
           </div>
           <div className="profileDetials">
             <div className="detials">
-              <Typography
-                variant="h4"
-                component="h1"
-                className="profileHeading"
-              >
+              <p component="h1" className="profileHeading">
                 MY DETAILS
-              </Typography>
-              <Typography className="profileText">The Monkey</Typography>
-              <Typography className="profileText">goyalyash1211@gmail.com</Typography>
-              <Typography className="profileText"> PHONE NUMBER</Typography>
-              <Typography className="profileText">Male</Typography>
+              </p>
+              <p className="profileText">The Monkey</p>
+              <p className="profileText">goyalyash1211@gmail.com</p>
+              <p className="profileText"> PHONE NUMBER</p>
+              <p className="profileText">Male</p>
             </div>
 
             <Link to="/profile/update" style={{ textDecoration: "none" }}>
-              <Button variant="contained" className="profileButton">
-                EDIT DETAILS
-              </Button>
+              <button className="profilebutton">EDIT DETAILS</button>
             </Link>
             <div className="detials">
-              <Typography
-                variant="h4"
+              <p
                 component="h1"
                 className="profileHeading"
                 style={{ marginTop: "1.5rem" }}
               >
                 LOGIN DETAILS
-              </Typography>
-              <Typography className="profileSubHeading">EMAIL</Typography>
-              <Typography className="profileText">goyalyash1211@gmail.com</Typography>
+              </p>
+              <p className="profileSubHeading">EMAIL</p>
+              <p className="profileText">goyalyash1211@gmail.com</p>
 
-              <Typography
-                className="profileSubHeading"
-                style={{ marginTop: "10px" }}
-              >
+              <p className="profileSubHeading" style={{ marginTop: "10px" }}>
                 PASSWORD
-              </Typography>
-              <Typography className="profileSubHeading">
-                *************
-              </Typography>
+              </p>
+              <p className="profileSubHeading">*************</p>
             </div>
             <Link
               to="/password/update"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              <Button variant="contained" className="profileButton">
-                UPDATE PASSWORD
-              </Button>
+              <button className="profilebutton">UPDATE PASSWORD</button>
             </Link>
 
             <div className="mangeAccount">
-              <Typography
-                variant="h4"
-                component="h1"
-                className="profileHeading"
-              >
+              <p component="h1" className="profileHeading">
                 Log out from all devices
-              </Typography>
+              </p>
 
               <p className="profileText3">
                 To access the Cricket Weapon Store website again, you need to
@@ -175,15 +127,9 @@ export const Profile = () => {
                 other web browsers you have used before.
               </p>
             </div>
-            <Button
-              variant="contained"
-              color="primary"
-              className="profileButton"
-              startIcon={<LogoutIcon />}
-              
-            >
+            <button color="primary" className="profilebutton">
               Logout Account
-            </Button>
+            </button>
           </div>
         </div>
       </div>
