f898f15f8a54143d04c4a42de187121557d1f86d
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2024-02-04 18:38:05 +0530

    CRA to vite conversion (#124)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,8 @@
-/** / @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      width: {
-        150: "150px",
-        190: "190px",
-        225: "225px",
-        275: "275px",
-        300: "300px",
-        340: "340px",
-        350: "350px",
-        375: "375px",
-        460: "460px",
-        656: "656px",
-        880: "880px",
-        508: "508px",
-      },
-      height: {
-        80: "80px",
-        150: "150px",
-        225: "225px",
-        300: "300px",
-        340: "340px",
-        370: "370px",
-        420: "420px",
-        510: "510px",
-        600: "600px",
-        650: "650px",
-        685: "685px",
-        800: "800px",
-        "90vh": "90vh",
-      },
-      minWidth: {
-        210: "210px",
-        350: "350px",
-        620: "620px",
-      },
-      screen: {
-        xs: "375px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        xi: "1440px",
-        "2xl": "1536px",
-      },
-      variants: {
-        display: ["responsive", "scrollbar"],
-      },
-      fontFamily: {
-        dance: "Dancing Script",
-      },
-      colors: {
-        offWhite: "#FFFAFA",
-        lightBlack: "#343434",
-      },
-    },
+    extend: {},
   },
-
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [],
 };
