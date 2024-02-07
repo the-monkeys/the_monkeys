9a9f23bbde92142a2db65340f4617d2566c5fe1c
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2024-02-07 11:39:15 +0530

    react to nextjs conversion

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-extrabold text-gray-800 mb-4">
+        We are Coming Soon!
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        Our Website is under construction, feel free to explore!
+      </p>
+      <p className="text-gray-700 mb-12">
+        Follow us and be the first to know when we go live!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
