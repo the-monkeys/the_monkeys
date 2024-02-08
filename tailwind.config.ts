f63afeaea3b23d25f05e83669d120f3162473e6e
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2024-02-08 18:57:55 +0530

    dark light mode setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,24 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    colors: {
+      primary: {
+        monkeyOrange: "#ff462e",
+        monkeyBlack: "#101010",
+        monkeyWhite: "#FFF4ed",
+      },
+      secondary: {
+        darkGray: "#2b2b2b",
+        lightGray: "#4f4f4f",
+        white: "#f2f2f3",
+      },
+      alert: {
+        red: "#F22020",
+        green: "#34A853",
+      },
+    },
     fontFamily: {
       jost: ["var(--font-jost)"],
       josefin_Sans: ["var(--font-josefin_Sans)"],
