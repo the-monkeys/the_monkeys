df77b572348bf7df6daace47e2b385ee80d3e1c1
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2024-02-16 13:21:41 +0530

    bug fixed

--- src/components/basic/ThemeSwitch.tsx
+++ src/components/basic/ThemeSwitch.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import Image from "next/image";
-
-import Icon from "../icon/index";
+import { useEffect, useState } from "react";
+import Icon from "../icon";
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
@@ -12,24 +11,24 @@ export default function ThemeSwitch() {
 
   useEffect(() => setMounted(true), []);
 
-	if (!mounted)
-		return (
-			<Image
-				src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='red'%3E%3Cpath d='M4 3H20C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3ZM5 5V19H19V5H5Z'%3E%3C/path%3E%3C/svg%3E"
-				width={24}
-				height={24}
-				sizes="24x24"
-				alt="Loading Light/Dark Toggle"
-				priority={false}
-				title="Loading Light/Dark Toggle"
-			/>
-		);
+  if (!mounted)
+    return (
+      <Image
+        src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='red'%3E%3Cpath d='M4 3H20C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3ZM5 5V19H19V5H5Z'%3E%3C/path%3E%3C/svg%3E"
+        width={24}
+        height={24}
+        sizes="24x24"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      />
+    );
 
-	if (resolvedTheme === "dark") {
-		return <Icon name="RiToggleLine" onClick={() => setTheme("light")} />;
-	}
+  if (resolvedTheme === "dark") {
+    return <Icon name="RiToggleLine" onClick={() => setTheme("light")} />;
+  }
 
-	if (resolvedTheme === "light") {
-		return <Icon name="RiToggleFill" onClick={() => setTheme("dark")} />;
-	}
+  if (resolvedTheme === "light") {
+    return <Icon name="RiToggleFill" onClick={() => setTheme("dark")} />;
+  }
 }
--- src/components/button/CircularButton.tsx
+++ src/components/button/CircularButton.tsx
@@ -1,36 +1,36 @@
-import Icon, { IconName } from "../icon/index";
+import Icon, { IconName } from "../icon";
 
 type CircularButtonProps = {
-	iconName: IconName;
-	title?: string;
-	animate?: boolean;
-	disabled?: boolean;
-	onClick?: () => void;
+  iconName: IconName;
+  title?: string;
+  animate?: boolean;
+  disabled?: boolean;
+  onClick?: () => void;
 };
 
 const CircularButton: React.FC<CircularButtonProps> = ({
-	iconName = "RiPencilLine",
-	title,
-	animate,
-	disabled,
-	onClick,
+  iconName = "RiPencilLine",
+  title,
+  animate,
+  disabled,
+  onClick,
 }) => {
-	return (
-		<div
-			className="group w-8 h-8 cur rounded-full flex items-center justify-center bg-primary-monkeyOrange cursor-pointer transition-all"
-			onClick={onClick}
-		>
-			<div className={`${animate ? "group-hover:animate-shake" : ""}`}>
-				<Icon
-					name={iconName}
-					size={18}
-					customColor={true}
-					color="#FFF4ed"
-					hasHover={false}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="group w-8 h-8 cur rounded-full flex items-center justify-center bg-primary-monkeyOrange cursor-pointer transition-all"
+      onClick={onClick}
+    >
+      <div className={`${animate ? "group-hover:animate-shake" : ""}`}>
+        <Icon
+          name={iconName}
+          size={18}
+          customColor={true}
+          color="#FFF4ed"
+          hasHover={false}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CircularButton;
