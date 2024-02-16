42ad03f9e623338a75f3a3aa061cadccf81d1354
github_piyush2108 <piyusharmap@gmail.com>
2024-02-16 11:10:42 +0530

    Added a subtle shake animation for icon button and added some more icons.

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,45 +1,83 @@
-import { FC } from "react";
+// Remix Icons: https://remixicon.com/
+// Github: https://github.com/Remix-Design/RemixIcon
+
 import * as RemixIcons from "@remixicon/react";
 
-type IconProps = {
-	title: string;
-	name:
-		| "RiToggleLine"
-		| "RiToggleFill"
-		| "RiNotification3Line"
-		| "RiUserLine"
-		| "RiPencilLine"
-		| "RiSearch2Line"
-		| "RiBookmarkLine"
-		| "RiArrowRightUpLine"
-		| "RiMoreLine"
-		| "RiCloseLine"
-		| "RiAddLine"
-		| "RiMailFill"
-		| "RiShareForwardFill"
-		| "RiShareForwardLine"
-		| "RiEyeLine"
-		| "RiEyeFill"
-		| "RiCake2Fill"
-		| "RiGithubFill"
-		| "RiTwitterFill"
-		| "RiGoogleFill";
+export type IconName =
+	| "RiToggleLine"
+	| "RiToggleFill"
+	| "RiNotification3Line"
+	| "RiUser3Line"
+	| "RiPencilLine"
+	| "RiSearchLine"
+	| "RiBookmarkLine"
+	| "RiArrowRightUpLine"
+	| "RiMoreLine"
+	| "RiCloseLine"
+	| "RiAddLine"
+	| "RiMailFill"
+	| "RiShareForwardFill"
+	| "RiShareForwardLine"
+	| "RiEyeLine"
+	| "RiEyeFill"
+	| "RiCake2Fill"
+	| "RiGithubFill"
+	| "RiTwitterXFill"
+	| "RiDiscordFill"
+	| "RiGoogleFill"
+	| "RiAlertLine"
+	| "RiErrorWarningLine";
+
+export type IconProps = {
+	name: IconName;
 	size?: number;
-	color: string;
+	hasHover?: boolean;
+	customColor?: boolean;
+	color?: string;
+	toolTip?: boolean;
+	toolTipSide?: "top" | "right" | "bottom" | "left";
 	onClick?: () => void;
 };
 
-const Icon: FC<IconProps> = ({ title, name, size, color, onClick }) => {
+const Icon: React.FC<IconProps> = ({
+	name,
+	size = 24,
+	hasHover = true,
+	customColor,
+	color,
+	toolTip,
+	toolTipSide,
+	onClick,
+}) => {
 	const DynamicIcon = RemixIcons[name];
 
 	return (
 		<div className="flex items-center justify-center">
-			<DynamicIcon
-				className="cursor-pointer hover:opacity-80"
-				size={size || 24}
-				color={color || "#ff462e"}
-				onClick={onClick}
-			/>
+			{customColor ? (
+				<DynamicIcon
+					className={`${hasHover && "hover:opacity-80"}`}
+					size={size}
+					onClick={onClick}
+					color={color}
+				/>
+			) : (
+				<>
+					<DynamicIcon
+						className={`cursor-pointer ${
+							hasHover && "hover:opacity-80"
+						} dark:hidden`}
+						size={size}
+						onClick={onClick}
+					/>
+					<DynamicIcon
+						className={`cursor-pointer ${
+							hasHover && "hover:opacity-80"
+						} dark:block hidden`}
+						size={size}
+						onClick={onClick}
+					/>
+				</>
+			)}
 		</div>
 	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,57 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
-  theme: {
-    colors: {
-      primary: {
-        monkeyOrange: "#ff462e",
-        monkeyBlack: "#101010",
-        monkeyWhite: "#FFF4ed",
-      },
-      secondary: {
-        darkGray: "#2b2b2b",
-        lightGray: "#4f4f4f",
-        white: "#f2f2f3",
-      },
-      alert: {
-        red: "#F22020",
-        green: "#34A853",
-      },
-    },
-    fontFamily: {
-      jost: ["var(--font-jost)"],
-      josefin_Sans: ["var(--font-josefin_Sans)"],
-      playfair_Display: ["var(--font-playfair_Display)"],
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+		colors: {
+			primary: {
+				monkeyOrange: "#ff462e",
+				monkeyBlack: "#101010",
+				monkeyWhite: "#FFF4ed",
+			},
+			secondary: {
+				darkGrey: "#2b2b2b",
+				lightGrey: "#4f4f4f",
+				white: "#f2f2f3",
+			},
+			alert: {
+				red: "#F22020",
+				green: "#34A853",
+			},
+		},
+		fontFamily: {
+			jost: ["var(--font-jost)"],
+			josefin_Sans: ["var(--font-josefin_Sans)"],
+			playfair_Display: ["var(--font-playfair_Display)"],
+		},
+		animation: {
+			shake: "shake 800ms ease-in-out",
+		},
+		keyframes: {
+			shake: {
+				"0%": {
+					transform: "rotate(0deg)",
+				},
+				"25%": {
+					transform: "rotate(15deg)",
+				},
+				"50%": {
+					transform: "rotate(-10deg)",
+				},
+				"75%": {
+					transform: "rotate(5deg)",
+				},
+				"100%": {
+					transform: "rotate(0)",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
