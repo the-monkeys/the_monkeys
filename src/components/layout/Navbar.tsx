67319812588207f214fd37e765a82449da7d67e8
Piyush <75241694+piyusharmap@users.noreply.github.com>
2024-02-16 19:39:21 +0530

    Added a Button Component (#133)
    * Replaced all .png logo files with .svg.
    
    * Added mix theme logos and removed old .png logo files.
    
    * Added a button component.
    
    * Created a proper layout for logo component for clean code.
    
    * Added a subtle shake animation for icon button and added some more icons.
    
    * Changed the folder structure for better understanding and clean code.
    
    * Minor changes in import
    
    * Changes in import statement for icon
    
    * bug fixed import of icon
    
    * bug fixed
    
    * change path
    
    * file name change
    
    ---------
    
    Co-authored-by: Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import {
-  RiAddLine,
-  RiGenderlessLine,
-  RiNotification3Line,
-  RiPencilLine,
-  RiUser3Line,
-} from "@remixicon/react";
 import { useState } from "react";
-import { AiOutlineMinus } from "react-icons/ai";
-import Logo from "../basic/Logo";
+
 import ThemeSwitch from "../basic/ThemeSwitch";
+import Button from "../button";
+import Icon from "../icon/icon";
+import Logo from "../logo";
 
 const Navbar = () => {
   const [isInputVisible, setIsInputVisible] = useState(false);
@@ -20,15 +15,13 @@ const Navbar = () => {
   };
 
   return (
-    <header className="flex gap-5  justify-between px-5 py-4 w-full text-base max-md:flex-wrap max-md:max-w-full bg-primary-monkeyWhite dark:bg-primary-monkeyBlack">
+    <header className="flex gap-5 justify-between px-5 py-4 w-full text-base max-md:flex-wrap max-md:max-w-full bg-primary-monkeyWhite dark:bg-primary-monkeyBlack">
       <div className="flex gap-5 justify-between items-center">
         <div>
           <Logo showMobileLogo={true} />
         </div>
-        <div className="transform rotate-45">
-          <div className="cursor-pointer" onClick={handleToggle}>
-            <RiGenderlessLine className="transform rotate-90" />
-          </div>
+        <div>
+          <Icon name="RiSearchLine" onClick={handleToggle} />
         </div>
         <div>
           {isInputVisible && (
@@ -38,27 +31,23 @@ const Navbar = () => {
                 className="border-b border-gray-300 w-64 focus:outline-none focus:border-blue-500 px-4 py-2"
                 placeholder="Enter text"
               />
-              <div className="absolute right-2 top-2 transform rotate-45">
-                <RiAddLine className="cursor-pointer text-gray-500" />
-              </div>
             </div>
           )}
         </div>
       </div>
 
       <div className="flex gap-5 justify-between items-center whitespace-nowrap text-zinc-100">
-        <RiNotification3Line />
+        <Icon name="RiNotification3Line" />
         <ThemeSwitch />
-        <RiUser3Line />
-        <AiOutlineMinus
-          className="cursor-pointer text-gray-500 transform rotate-90"
-          size={30}
-        />
-        <div>
-          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-primary-monkeyOrange">
-            <RiPencilLine />
-          </div>
-          <div>Create</div>
+        <Icon name="RiUser3Line" />
+        <div className="flex flex-col items-center">
+          <Button
+            title="Create"
+            variant="circular"
+            iconName="RiPencilLine"
+            animate={true}
+          />
+          <p className="font-playfair_Display font-medium">Create</p>
         </div>
       </div>
     </header>
