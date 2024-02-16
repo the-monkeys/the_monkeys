5e85cad80f7a3c75a9ef4c4127d0396894ce4eaf
github_piyush2108 <piyusharmap@gmail.com>
2024-02-16 11:38:32 +0530

    Minor changes in import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,43 +2,47 @@ import type { Metadata } from "next";
 import { Josefin_Sans, Jost, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { ThemeProviders } from "./theme-provider";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const jost = Jost({
-  subsets: ["latin"],
-  variable: "--font-jost",
-  display: "swap",
+	subsets: ["latin"],
+	variable: "--font-jost",
+	display: "swap",
 });
 
 const josefin_Sans = Josefin_Sans({
-  subsets: ["latin"],
-  variable: "--font-josefin_Sans",
-  display: "swap",
+	subsets: ["latin"],
+	variable: "--font-josefin_Sans",
+	display: "swap",
 });
 const playfair_Display = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair_Display",
-  display: "swap",
+	subsets: ["latin"],
+	variable: "--font-playfair_Display",
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Monkeys",
-  description: "We are the Monkeys!",
+	title: "Monkeys",
+	description: "We are the Monkeys!",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${jost.variable} ${josefin_Sans.variable}  ${playfair_Display.variable}`}
-      >
-        <ThemeProviders>
-          <main>{children}</main>
-        </ThemeProviders>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${jost.variable} ${josefin_Sans.variable}  ${playfair_Display.variable}`}
+			>
+				<ThemeProviders>
+					<Navbar />
+					<main>{children}</main>
+					<Footer />
+				</ThemeProviders>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Navbar from "@/components/layout/Navbar";
 import ComingSoon from "../components/ComingSoon";
-import Footer from "@/components/layout/Footer";
 
 const Home = () => {
 	return (
 		<>
-			<Navbar />
 			<div className="flex justify-center bg-primary-monkeyWhite dark:bg-primary-monkeyBlack">
 				<ComingSoon />
 			</div>
-			<Footer />
 		</>
 	);
 };
