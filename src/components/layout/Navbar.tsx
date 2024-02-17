527537bdd3ad1ee53308b0ff6443e4b114e272e3
github_piyush2108 <piyusharmap@gmail.com>
2024-02-17 13:36:47 +0530

    Added new input components to footer and navbar.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,52 +6,52 @@ import ThemeSwitch from "../basic/ThemeSwitch";
 import Button from "../button";
 import Icon from "../icon/icon";
 import Logo from "../logo";
+import Input from "../input";
 
 const Navbar = () => {
-  const [isInputVisible, setIsInputVisible] = useState(false);
+	const [searchInput, setSearchInput] = useState<string>("");
+	const [inputVisibility, setInputVisibility] = useState<boolean>(false);
 
-  const handleToggle = () => {
-    setIsInputVisible((prev) => !prev);
-  };
+	const handleInputToggle = () => {
+		setInputVisibility((prevVal) => !prevVal);
+	};
 
-  return (
-    <header className="flex gap-5 justify-between px-5 py-4 w-full text-base max-md:flex-wrap max-md:max-w-full bg-primary-monkeyWhite dark:bg-primary-monkeyBlack">
-      <div className="flex gap-5 justify-between items-center">
-        <div>
-          <Logo showMobileLogo={true} />
-        </div>
-        <div>
-          <Icon name="RiSearchLine" onClick={handleToggle} />
-        </div>
-        <div>
-          {isInputVisible && (
-            <div className="flex items-center relative transform rotate-15">
-              <input
-                type="text"
-                className="border-b border-gray-300 w-64 focus:outline-none focus:border-blue-500 px-4 py-2"
-                placeholder="Enter text"
-              />
-            </div>
-          )}
-        </div>
-      </div>
+	return (
+		<header className="p-4 w-full flex justify-between">
+			<div className="w-2/5 flex items-center gap-4">
+				<div>
+					<Logo showMobileLogo={true} />
+				</div>
+				<div>
+					<Icon name="RiSearchLine" onClick={handleInputToggle} />
+				</div>
+				{inputVisibility && (
+					<div className="flex-1">
+						<Input
+							variant="search"
+							placeholderText="search here"
+							setInputText={setSearchInput}
+						/>
+					</div>
+				)}
+			</div>
 
-      <div className="flex gap-5 justify-between items-center whitespace-nowrap text-zinc-100">
-        <Icon name="RiNotification3Line" />
-        <ThemeSwitch />
-        <Icon name="RiUser3Line" />
-        <div className="flex flex-col items-center">
-          <Button
-            title="Create"
-            variant="circular"
-            iconName="RiPencilLine"
-            animate={true}
-          />
-          <p className="font-playfair_Display font-medium">Create</p>
-        </div>
-      </div>
-    </header>
-  );
+			<div className="flex gap-5 justify-between items-center">
+				<Icon name="RiNotification3Line" />
+				<ThemeSwitch />
+				<Icon name="RiUser3Line" />
+				<div className="flex flex-col items-center">
+					<Button
+						title="Create"
+						variant="circular"
+						iconName="RiPencilLine"
+						animate={true}
+					/>
+					<p className="font-playfair_Display font-medium">Create</p>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
