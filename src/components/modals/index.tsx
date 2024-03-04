8e8382c20eb0c44e4ec44615e305bc15f55d791c
Procax <babablack626@gmail.com>
2024-03-04 21:21:48 +0530

    logo fix

--- src/components/modals/index.tsx
+++ src/components/modals/index.tsx
@@ -12,7 +12,10 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, setModal }) => {
       className='fixed left-0 top-0 flex h-full w-full items-center justify-center bg-secondary-white/60 backdrop-blur-sm dark:bg-secondary-darkGrey/60'
       onClick={() => setModal(false)}
     >
-      <div className='flex w-full flex-col items-start gap-1 sm:max-w-md sm:self-center'>
+      <div className='flex w-full flex-col items-start gap-1 self-end sm:max-w-md sm:self-center'>
+        <div className='ml-2'>
+          <Logo showMix />
+        </div>
         <div className='flex h-[60vh] w-full flex-col gap-2 rounded-lg bg-primary-monkeyWhite transition-all dark:bg-primary-monkeyBlack sm:h-[500px] sm:self-center'>
           {children}
         </div>
--- src/components/modals/layout/ModalHeader.tsx
+++ src/components/modals/layout/ModalHeader.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import Icon from '../../icon';
-import Logo from '@/components/logo';
 
 type ModalHeaderProps = {
   heading?: string;
@@ -15,15 +14,9 @@ const ModalHeader: FC<ModalHeaderProps> = ({
 }) => {
   return (
     <div className='flex flex-col justify-center gap-4'>
-      <div className='flex flex-row justify-between'>
-        <div className='ml-6 mt-3'>
-          <Logo showMix />
-        </div>
-        <div className='m-2 mb-6'>
-          <Icon name='RiCloseLine' size={20} onClick={() => setModal(false)} />
-        </div>
+      <div className='m-2 flex justify-end'>
+        <Icon name='RiCloseLine' size={20} onClick={() => setModal(false)} />
       </div>
-
       {showHeading && (
         <p className='text-center font-playfair_Display text-2xl font-semibold'>
           {heading}
