7bbcfe7843241ecb1082438ab04b63bd38cef303
github_piyush2108 <piyusharmap@gmail.com>
2024-03-08 00:57:23 +0530

    Fixed height of notification dialog

--- src/components/layout/navbar/nav/notifications/NotificationContent.tsx
+++ src/components/layout/navbar/nav/notifications/NotificationContent.tsx
@@ -1,6 +1,6 @@
 const NotificationContent = () => {
   return (
-    <div className='flex h-[500px] max-h-[60vh] flex-col overflow-auto border-t-1 border-secondary-lightGrey/25 px-4 py-2'>
+    <div className='flex h-[50vh] flex-col overflow-auto border-t-1 border-secondary-lightGrey/25 px-4 py-2'>
       <p className='text-center font-jost text-lg'>No notifications yet!</p>
     </div>
   );
