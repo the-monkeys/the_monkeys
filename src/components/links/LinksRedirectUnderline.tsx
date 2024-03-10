6594e156e032e2f5d4bf01095427530209011f44
github_piyush2108 <piyusharmap@gmail.com>
2024-03-10 19:31:40 +0530

    Added list component for footer

---
+++ src/components/links/LinksRedirectUnderline.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import Link from 'next/link';
+
+import { twMerge } from 'tailwind-merge';
+
+type LinksRedirectProps = {
+  target: string;
+  title: string;
+  className?: string;
+};
+
+const LinksRedirectUnderline: FC<LinksRedirectProps> = ({
+  target,
+  title,
+  className,
+}) => {
+  return (
+    <Link
+      href={target}
+      target='_blank'
+      className={twMerge(className, 'group flex flex-col')}
+    >
+      <p className='font-jost text-secondary-lightGrey'>{title}</p>
+      <span className='h-[1px] w-0 bg-secondary-lightGrey transition-all group-hover:w-full'></span>
+    </Link>
+  );
+};
+
+export default LinksRedirectUnderline;
