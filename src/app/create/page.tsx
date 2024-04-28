121ba46f30049cfc6ab45d8a80d914b8a8a6765e
github_piyush2108 <piyusharmap@gmail.com>
2024-04-29 01:09:27 +0530

    Added blog preview functionality

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -5,6 +5,7 @@ import React, { Suspense, useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 import Button from '@/components/button';
+import EditorPreview from '@/components/editor/Preview';
 // import Editor, { EditorProps } from '@/components/editor';
 import PublishModal from '@/components/modals/publish/PublishModal';
 import { OutputData } from '@editorjs/editorjs';
@@ -25,6 +26,8 @@ const initial_data = {
   ],
 };
 
+export type EditorState = 'Edit' | 'Preview';
+
 type EditorProps = {
   data: OutputData;
   onChange?: (data: OutputData) => void;
@@ -34,6 +37,7 @@ function App() {
   const [editor, setEditor] = useState<React.FC<EditorProps> | null>(null);
   const [data, setData] = useState<OutputData>(initial_data);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [editorState, setEditorState] = useState<EditorState>('Edit');
 
   useEffect(() => {
     const loadEditor = async () => {
@@ -44,21 +48,36 @@ function App() {
     loadEditor();
   }, []);
 
+  const handleEditorState = () => {
+    if (editorState === 'Edit') {
+      setEditorState('Preview');
+    } else {
+      setEditorState('Edit');
+    }
+  };
+
   return (
     <div>
-      <div className='px-5 sm:px-4 py-2 flex gap-4 items-center justify-end'>
-        <div className='flex gap-4 items-center justify-between'>
-          <Button
-            title='Publish Blog'
-            variant='primary'
-            onClick={() => setShowModal(true)}
-          />
-        </div>
+      <div className='px-5 sm:px-4 py-2 flex gap-2 items-center justify-end'>
+        <Button
+          title={editorState === 'Edit' ? 'Preview' : 'Edit'}
+          variant='secondary'
+          onClick={handleEditorState}
+        />
+        <Button
+          title='Publish Blog'
+          variant='primary'
+          onClick={() => setShowModal(true)}
+        />
       </div>
 
       <Suspense fallback={<p>Loading...</p>}>
-        {editor && <Editor data={data} onChange={setData} />}
+        {editor && editorState === 'Edit' && (
+          <Editor data={data} onChange={setData} />
+        )}
       </Suspense>
+
+      {editorState === 'Preview' && <EditorPreview data={data} />}
 
       {showModal && <PublishModal setModal={setShowModal} />}
     </div>
