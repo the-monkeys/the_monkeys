60ba04e70a9d5b6ed050e134dc364e8bd1c4eae6
Rahullokare <lokarerahul55@gmail.com>
2024-05-24 12:13:09 +0530

    Notifications added

--- src/app/settings/components/Notifications.tsx
+++ src/app/settings/components/Notifications.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  return (
+    <div className='flex justify-center items-center h-[70vh]'>
+      <p className='text-secondary-mute italic text-2xl '>
+        No notifications yet
+      </p>
+    </div>
+  );
 };
 
 export default Notifications;
