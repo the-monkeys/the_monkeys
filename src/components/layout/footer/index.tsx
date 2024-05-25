a28f74d1a7b17c201e8c4171668582dc0f600793
Nwosa <obere4u@gmail.com>
2024-05-25 15:23:22 +0100

    Made the year on footer to be dynamic

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -15,6 +15,9 @@ import List from './list';
 const Footer = () => {
   const [userMail, setUserMail] = useState<string>('');
 
+  /* Get the current year dynamically */
+  const currentYear = new Date().getFullYear() 
+
   return (
     <footer className='flex flex-col gap-10 border-t-1 border-secondary-lightGrey/25 px-5 pb-4 pt-10'>
       <div className='item-start flex flex-col justify-between gap-10 md:flex-row md:items-end'>
@@ -40,6 +43,7 @@ const Footer = () => {
         </div>
       </div>
 
+      {/*Footer Icons*/}
       <div className='mb-14 flex w-fit flex-col items-center gap-2 self-center'>
         <div className='flex items-center justify-center gap-2'>
           <Link
@@ -65,7 +69,7 @@ const Footer = () => {
           </Link>
         </div>
         <p className='w-fit font-josefin_Sans text-xs text-secondary-lightGrey'>
-          Monkeys, 2024, All Rights Reserved
+          Monkeys, {currentYear}, All Rights Reserved
         </p>
       </div>
     </footer>
