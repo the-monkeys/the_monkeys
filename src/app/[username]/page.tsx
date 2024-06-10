5548cae03b03e6eb47982c34e60553e43eed2c8d
Nwosa <obere4u@gmail.com>
2024-06-10 07:27:52 +0100

    Replaced Activity Link with Post and Draft Tabs

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,11 +1,24 @@
 import LinksRedirectArrow from '@/components/links/LinksRedirectArrow';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const UserPosts = () => {
   return (
     <div className='px-5 py-4'>
-      <div className='flex justify-end'>
-        <LinksRedirectArrow target='/activity' title='View Activity' />
-      </div>
+      {/*Remove this 
+        <LinksRedirectArrow target='/activity' title='View Activity' />*/}
+      {/*Add tabs to switch between Posts and Drafts */}
+      <Tabs>
+        <div className='flex justify-end'>
+          <TabsList>
+            <TabsTrigger value='posts'>
+              <p>Posts</p>
+            </TabsTrigger>
+            <TabsTrigger value='drafts'>
+              <p>Draft</p>
+            </TabsTrigger>
+          </TabsList>
+        </div>
+      </Tabs>
     </div>
   );
 };
