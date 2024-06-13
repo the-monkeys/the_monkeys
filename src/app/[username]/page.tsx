d02618a6f6cb3bfe4e4842631a31f04470f5d002
Nwosa <obere4u@gmail.com>
2024-06-13 10:08:55 +0100

    Edit the TabContent of th pages.

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,5 +1,8 @@
 import LinksRedirectArrow from '@/components/links/LinksRedirectArrow';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import All from './components/All';
+import Collab from './components/Collab';
 
 const UserPosts = () => {
   return (
@@ -10,13 +13,25 @@ const UserPosts = () => {
       <Tabs>
         <div className='flex justify-end'>
           <TabsList>
-            <TabsTrigger value='posts'>
-              <p>Posts</p>
+            <TabsTrigger className='text-xl' value='all'>
+              <p className='font-josefin_Sans text-base sm:text-xl'>All</p>
+              <div className='h-[2px] w-1 bg-primary-monkeyOrange group-hover:w-full group-data-[state=active]:w-full transition-all rounded-full' />
             </TabsTrigger>
-            <TabsTrigger value='drafts'>
-              <p>Draft</p>
+            <TabsTrigger className='text-xl' value='collab'>
+              <p className='font-josefin_Sans text-base sm:text-xl'>Collab</p>
+              <div className='h-[2px] w-1 bg-primary-monkeyOrange group-hover:w-full group-data-[state=active]:w-full transition-all rounded-full' />
             </TabsTrigger>
           </TabsList>
+        </div>
+
+        <div className='mx-auto w-full md:w-4/5'>
+          <TabsContent className='w-full' value='all'>
+            <All />
+          </TabsContent>
+
+          <TabsContent className='w-full' value='collab'>
+            <Collab />
+          </TabsContent>
         </div>
       </Tabs>
     </div>
