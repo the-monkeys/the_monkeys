8ac3244acdfcffe5cb11329b1529198480c0c634
Nwosa <obere4u@gmail.com>
2024-06-13 10:08:55 +0100

    Edit the TabContent of th pages.

---
+++ src/app/[username]/components/All.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const All = () => {
+  return (
+    <div className='flex justify-center items-center h-[70vh]'>
+      <p className='font-jost italic text-2xl '>List is currently empty.</p>
+    </div>
+  );
+};
+
+export default All;
---
+++ src/app/[username]/components/Collab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Collab = () => {
+  return (
+    <div className='flex justify-center items-center h-[70vh]'>
+      <p className='font-jost italic text-2xl '>List is currently empty.</p>
+    </div>
+  );
+};
+
+export default Collab;
