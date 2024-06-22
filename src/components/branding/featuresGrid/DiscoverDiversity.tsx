463b8c64e933c2c282536895c7044ba66b2fad35
github_piyush2108 <piyusharmap@gmail.com>
2024-06-23 02:04:28 +0530

    changed content in version control grid

--- src/components/branding/featuresGrid/DiscoverDiversity.tsx
+++ src/components/branding/featuresGrid/DiscoverDiversity.tsx
@@ -21,7 +21,7 @@ const DiscoverDiversity = () => {
       </GridSubHeading>
 
       <div className='mt-4 flex justify-center gap-1 h-44 overflow-hidden'>
-        <div className='-mt-2 group-hover:-mt-16 w-fit space-y-1 transition-all hidden sm:block'>
+        <div className='-mt-2 group-hover:-mt-16 w-fit space-y-2 transition-all hidden sm:block'>
           <div className='w-32 sm:w-36 h-10 bg-secondary-darkGrey/10 dark:bg-secondary-white/10 rounded-full' />
           <div className='w-32 sm:w-36 h-10 flex items-center justify-center font-jost text-sm sm:text-base text-secondary-white dark:text-secondary-darkGrey bg-primary-monkeyBlack dark:bg-primary-monkeyWhite rounded-full'>
             Technology
@@ -34,7 +34,7 @@ const DiscoverDiversity = () => {
           <div className='w-32 sm:w-36 h-10 bg-secondary-darkGrey/10 dark:bg-secondary-white/10 rounded-full' />
         </div>
 
-        <div className='-mt-16 group-hover:-mt-2 mr-4 w-fit space-y-1 transition-all hidden sm:block'>
+        <div className='-mt-16 group-hover:-mt-2 mr-4 w-fit space-y-2 transition-all hidden sm:block'>
           <div className='w-32 sm:w-36 h-10 bg-secondary-darkGrey/10 dark:bg-secondary-white/10 rounded-full' />
           <div className='w-32 sm:w-36 h-10 flex items-center justify-center font-jost text-sm sm:text-base text-secondary-white dark:text-secondary-darkGrey bg-primary-monkeyBlack dark:bg-primary-monkeyWhite rounded-full'>
             Science
--- src/components/branding/featuresGrid/VersionControl.tsx
+++ src/components/branding/featuresGrid/VersionControl.tsx
@@ -19,7 +19,51 @@ const VersionControl = () => {
         Make your blog uniquely yours with multiple personalized versions.
       </GridSubHeading>
 
-      <div className='mt-4 overflow-hidden'></div>
+      <div className='mt-4 flex-1 flex flex-col items-center justify-end overflow-hidden'>
+        <div className='flex-1 w-full px-4 py-3 border-t-2 border-primary-monkeyOrange rounded-t-2xl transition-all'>
+          <p className='font-josefin_Sans text-xl text-primary-monkeyOrange'>
+            Blog Published: v2.8
+          </p>
+          <p className='mb-2 font-jost opacity-75'>
+            by Rick Astley, Jake Peralta +1
+          </p>
+          <p className='mb-4 font-jost text-sm opacity-75 text-right'>
+            2 days ago
+          </p>
+
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-secondary-darkGrey/10 dark:bg-secondary-white/10' />
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-alert-red/25' />
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-alert-green/25' />
+          <div className='mb-3 h-2 sm:h-3 w-3/4 rounded-full bg-alert-green/25 group-hover:w-full transition-all' />
+
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-secondary-darkGrey/10 dark:bg-secondary-white/10' />
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-alert-red/25' />
+          <div className='mb-1 h-2 sm:h-3 w-1/2 rounded-full bg-alert-red/25 group-hover:w-full transition-all' />
+          <div className='mb-1 h-2 sm:h-3 w-full rounded-full bg-alert-green/25' />
+          <div className='mb-1 h-2 sm:h-3 w-1/4 rounded-full bg-alert-green/25 group-hover:w-full transition-all' />
+        </div>
+
+        <div className='w-full px-4 py-3 hover:pb-6 border-t-2 border-secondary-lightGrey/25 hover:border-secondary-lightGrey/75 rounded-t-2xl transition-all'>
+          <p className='font-josefin_Sans cursor-default'>v2.5</p>
+          <p className='font-jost text-sm opacity-75 cursor-default'>
+            updated 19 days ago
+          </p>
+        </div>
+
+        <div className='w-full px-4 py-3 hover:pb-6 border-t-2 border-secondary-lightGrey/25 hover:border-secondary-lightGrey/75 rounded-t-2xl transition-all'>
+          <p className='font-josefin_Sans cursor-default'>v2.2</p>
+          <p className='font-jost text-sm opacity-75 cursor-default'>
+            updated 27 days ago
+          </p>
+        </div>
+
+        <div className='w-full px-4 py-3 hover:pb-6 border-t-2 border-secondary-lightGrey/25 hover:border-secondary-lightGrey/75 rounded-t-2xl transition-all hidden md:block'>
+          <p className='font-josefin_Sans cursor-default'>v1.4</p>
+          <p className='font-jost text-sm opacity-75 cursor-default'>
+            updated 2 months ago
+          </p>
+        </div>
+      </div>
     </GridContainer>
   );
 };
