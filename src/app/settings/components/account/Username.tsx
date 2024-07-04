c2a5c9741a7f0c34025787f849ce77b2c99a2cb3
Procax <babablack626@gmail.com>
2024-07-04 11:57:28 +0530

    staged changes

--- src/app/settings/components/account/Username.tsx
+++ src/app/settings/components/account/Username.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios, { AxiosError } from 'axios';
 import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -17,8 +16,14 @@ import { updateUsername } from '@/lib/schema/settings';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { useSession } from 'next-auth/react';
+import { API_URL } from '@/constants/api';
 
 const Username = () => {
+  const { data } = useSession();
+  const [usernameExists, setUsernameExists] = useState<boolean | null>(null);
+  const [responseMessage, setResponseMessage] = useState<string | null>(null);
+
   const form = useForm<z.infer<typeof updateUsername>>({
     resolver: zodResolver(updateUsername),
     defaultValues: {
@@ -26,9 +31,32 @@ const Username = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof updateUsername>) {
-    console.log(values);
-  }
+  const checkUsernameExists = async (username: string) => {
+
+    try {
+      const response = await axios.get(`${API_URL}/user/${username}`, {
+        headers: {
+          Authorization: `Bearer ${data.user.token}`,
+        },
+      });
+      return { exists: response.status === 200, message: response.data?.message || 'Username exists' };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 404) {
+          console.log('Username does not exist');
+          return { exists: false, message: error.response.data?.message || 'Username does not exist' };
+        }
+      } else {
+      }
+      return { exists: true, message: 'Error checking username' }; // Assume true in case of error to avoid overwriting existing usernames
+    }
+  };
+
+  const onSubmit = async (values: z.infer<typeof updateUsername>) => {
+    const result = await checkUsernameExists(values.username);
+    setUsernameExists(result.exists);
+    setResponseMessage(result.message);
+  };
 
   return (
     <div className='flex flex-col items-start'>
@@ -54,6 +82,11 @@ const Username = () => {
                     <FormControl>
                       <Input placeholder='Enter username' {...field} />
                     </FormControl>
+                    {responseMessage && (
+                      <p className={`text-sm mt-1 text-primary-monkeyOrange ${usernameExists}`}>
+                        {responseMessage}
+                      </p>
+                    )}
                   </FormItem>
                 )}
               />
