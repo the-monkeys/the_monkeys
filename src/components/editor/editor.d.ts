ba8afc2254aaaba19784191741dc76aa2b8be620
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-08-27 15:56:23 +0530

    features: added list,quote and code block using editorjs

--- src/components/editor/editor.d.ts
+++ src/components/editor/editor.d.ts
@@ -12,3 +12,23 @@ declare module '@editorjs/image' {
   import Image from '@editorjs/image';
   export = Image;
 }
+
+declare module '@editorjs/list' {
+  import List from '@editorjs/list';
+  export = List;
+}
+
+declare module '@editorjs/code' {
+  import CodeTool from '@editorjs/code';
+  export = CodeTool;
+}
+
+declare module '@editorjs/nested-list' {
+  import NestedList from '@editorjs/nested-list';
+  export = App;
+}
+
+declare module '@editorjs/quote' {
+  import Quote from '@editorjs/quote';
+  export = Quote;
+}
--- src/config/editorjs.config.ts
+++ src/config/editorjs.config.ts
@@ -1,7 +1,11 @@
+import CodeTool from '@editorjs/code';
 import { EditorConfig } from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import Image from '@editorjs/image';
+import List from '@editorjs/list';
+import NestedList from '@editorjs/nested-list';
 import Paragraph from '@editorjs/paragraph';
+import Quote from '@editorjs/quote';
 
 export const editorConfig: EditorConfig = {
   holder: 'editorjs_editor-container',
@@ -10,7 +14,7 @@ export const editorConfig: EditorConfig = {
       class: Header,
       inlineToolbar: true,
       config: {
-        levels: [1, 2],
+        levels: [1, 2, 3],
         defaultLevel: 1,
       },
     },
@@ -28,6 +32,28 @@ export const editorConfig: EditorConfig = {
           byFile: '',
           byUrl: '',
         },
+      },
+    },
+    NestedList: {
+      class: NestedList,
+      inlineToolbar: true,
+      config: {
+        defaultStyle: 'ordered',
+      },
+    },
+    code: {
+      class: CodeTool,
+      inlineToolbar: true,
+      config: {
+        placeholder: 'Enter your code ...',
+      },
+    },
+    quote: {
+      class: Quote,
+      inlineToolbar: true,
+      config: {
+        quotePlaceholder: 'Enter a quote',
+        captionPlaceholder: "Quote's author",
       },
     },
   },
