68d5e8dd148c3cf8a4a72b30e56a9187eb541b90
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-09-17 23:07:23 +0530

    todays update

--- src/app/news/components/News1Card.tsx
+++ src/app/news/components/News1Card.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import Link from 'next/link';
 
+import Icon from '@/components/icon';
 import { Badge } from '@/components/ui/badge';
 import { NewsSource1 } from '@/services/news/newsTypes';
 import { newsDateFormatter } from '@/utils/dateFormatter';
@@ -39,9 +40,19 @@ const News1Card: FC<NewsSource1> = (props) => {
           </div>
         )}
 
-        <p className='font-josefin_Sans text-sm truncate opacity-75'>
-          {source}
-        </p>
+        <div className='flex justify-between align-center'>
+          <p className='font-josefin_Sans text-sm truncate opacity-75'>
+            {source}
+          </p>
+          <div className='flex justify-center items-center opacity-60'>
+            <Icon
+              name='RiTime'
+              size={16}
+              className='mx-2 transition-all group-hover:ml-3 group-hover:mr-1 '
+            />{' '}
+            {Math.ceil(description.length / 100) + ' min'}
+          </div>
+        </div>
 
         <h2 className='mt-1 font-josefin_Sans font-medium text-xl sm:text-2xl leading-tight line-clamp-2 group-hover:opacity-80'>
           {title}
@@ -55,7 +66,7 @@ const News1Card: FC<NewsSource1> = (props) => {
         <div className='mt-2 flex justify-between'>
           <Badge
             variant='secondary'
-            className='font-jost text-xs sm:text-sm capitalize'
+            className='font-jost text-xs sm:text-sm capitalize '
           >
             {category}
           </Badge>
