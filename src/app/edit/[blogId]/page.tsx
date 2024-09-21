556b62da5ff714c0032f98be8b5ebac395421f85
rahullokare <lokarerahul55@gmail.com>
2024-09-21 13:05:39 +0530

    Edit functionality added

---
+++ src/app/edit/[blogId]/page.tsx
@@ -0,0 +1,178 @@
+'use client';
+
+import React, { Suspense, useCallback, useEffect, useState } from 'react';
+
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+
+import { EditorProps } from '@/components/editor';
+import Icon from '@/components/icon';
+import Container from '@/components/layout/Container';
+import { Loader } from '@/components/loader';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+import useGetDraftBlogDetail from '@/hooks/useGetDraftBlogDetail';
+import axiosInstance from '@/services/api/axiosInstance';
+import { OutputData } from '@editorjs/editorjs';
+import { useSession } from 'next-auth/react';
+
+// Dynamically import the Editor component to avoid server-side rendering issues
+const Editor = dynamic(() => import('@/components/editor'), {
+  ssr: false,
+});
+
+export default function Page({ params }: { params: { blogId: string } }) {
+  const [editor, setEditor] = useState<React.FC<EditorProps> | null>(null);
+  const [data, setData] = useState<OutputData | null>(null);
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+  const { data: session } = useSession();
+  const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+  const router = useRouter();
+  const blogId = params.blogId;
+
+  // Fetch the blog data using the blogId
+  const { blogs, isLoading } = useGetDraftBlogDetail(
+    session?.user.account_id,
+    blogId
+  );
+
+  // Function to create and manage WebSocket connection
+  const createWebSocket = useCallback((blogId: string, token: string) => {
+    const ws = new WebSocket(
+      `wss://dev.themonkeys.site/api/v1/blog/draft/${blogId}?token=${token}`
+    );
+
+    ws.onopen = () => {
+      console.log('WebSocket connection opened');
+    };
+
+    ws.onmessage = (event) => {
+      console.log('WebSocket message received:', event.data);
+      setIsSaving(false); // Reset saving status when message is received
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    ws.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    setWebSocket(ws);
+
+    // Cleanup function to close the WebSocket connection
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  // Function to format data before sending it to the server
+  const formatData = useCallback(
+    (data: OutputData, accountId: string | undefined) => {
+      return {
+        owner_account_id: accountId,
+        blog: {
+          time: data.time,
+          blocks: data.blocks.map((block) => ({
+            ...block,
+            author: [accountId],
+            time: new Date().getTime(),
+          })),
+        },
+        tags: ['tech', 'nextjs', 'ui'],
+      };
+    },
+    []
+  );
+
+  // Handle the publish action
+  const handlePublishStep = useCallback(() => {
+    if (!data) return; // Ensure data is not null
+
+    const formattedData = formatData(data, session?.user.account_id);
+    axiosInstance
+      .post(`/blog/publish/${blogId}`, formattedData)
+      .then((res) => {
+        console.log(res);
+        toast({
+          variant: 'success',
+          title: 'Blog Published successfully',
+          description: 'success',
+        });
+        router.push(`/`);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast({
+          variant: 'destructive',
+          title: 'Error publishing blog',
+          description: 'error',
+        });
+      });
+  }, [data, session?.user.account_id, blogId, formatData, router]);
+
+  // Load the Editor component dynamically
+  useEffect(() => {
+    const loadEditor = async () => {
+      const module = await import('@/components/editor');
+      setEditor(() => module.default);
+    };
+
+    loadEditor();
+  }, []);
+
+  // Create WebSocket connection when authToken is available and blog data is loaded
+  useEffect(() => {
+    if (session?.user.token && blogs) {
+      const cleanup = createWebSocket(blogId, session.user.token);
+      setData(blogs.blog); // Set the editor data with the fetched blog data
+
+      // Listen for beforeunload event to close the WebSocket connection
+      const handleBeforeUnload = () => {
+        cleanup();
+      };
+
+      window.addEventListener('beforeunload', handleBeforeUnload);
+
+      return () => {
+        cleanup();
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+      };
+    }
+  }, [session?.user.token, blogId, createWebSocket, blogs]);
+
+  // Send data to WebSocket when data changes
+  useEffect(() => {
+    if (webSocket && webSocket.readyState === WebSocket.OPEN && data) {
+      const formattedData = formatData(data, session?.user.account_id);
+      webSocket.send(JSON.stringify(formattedData));
+      setIsSaving(true); // Set saving status when data is sent
+    }
+  }, [data, webSocket, session?.user.account_id, formatData]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <Container className='min-h-screen px-5 py-4 pb-12'>
+      <div className='space-y-2'>
+        <div className='flex justify-end gap-2'>
+          <Button variant='ghost' onClick={() => console.log(data)}>
+            Preview
+          </Button>
+
+          <Button onClick={handlePublishStep}>Publish</Button>
+        </div>
+        <div className='flex items-center gap-2'>
+          Saving draft{' '}
+          {isSaving ? <Loader /> : <Icon name='RiCheck' size={20} />}{' '}
+        </div>
+        <Suspense fallback={<Loader />}>
+          {editor && data && <Editor data={data} onChange={setData} />}
+        </Suspense>
+      </div>
+    </Container>
+  );
+}
