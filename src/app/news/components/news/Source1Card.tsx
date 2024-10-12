ea87bb0036c4d99569e06bba6e380f7626b173f0
rahullokare <lokarerahul55@gmail.com>
2024-10-12 17:29:21 +0530

    Issue Resolved NULL IMAGE

--- src/app/news/components/news/Source1Card.tsx
+++ src/app/news/components/news/Source1Card.tsx
@@ -27,18 +27,18 @@ export const Source1NewsCard1 = ({
   return (
     <div className={twMerge(className, 'group')}>
       <img
-        src={newsItem.image || ''}
-        alt={newsItem.source || ''}
+        src={newsItem?.image || ''}
+        alt={newsItem?.source || ''}
         loading='lazy'
         className='h-[200px] w-full object-cover group-hover:opacity-75'
       />
 
       <p className='mt-2 mb-1 font-jost text-xs sm:text-sm opacity-75'>
-        {newsItem.source}
+        {newsItem?.source}
       </p>
 
       <h2 className='font-josefin_Sans font-semibold line-clamp-2'>
-        {newsItem.title}
+        {newsItem?.title}
       </h2>
     </div>
   );
@@ -59,19 +59,19 @@ export const Source1NewsCard2 = ({
       )}
     >
       <img
-        src={newsItem.image || ''}
-        alt={newsItem.source || ''}
+        src={newsItem?.image || ''}
+        alt={newsItem?.source || ''}
         loading='lazy'
         className='sm:w-60 h-[200px] object-cover group-hover:opacity-75'
       />
 
       <div className='flex-1'>
         <p className='mb-2 font-jost text-xs sm:text-sm opacity-75'>
-          {newsItem.source}
+          {newsItem?.source}
         </p>
 
         <h2 className='font-josefin_Sans font-semibold text-lg line-clamp-2'>
-          {newsItem.title}
+          {newsItem?.title}
         </h2>
 
         <p
@@ -89,16 +89,16 @@ export const Source1NewsCard3 = ({ newsItem }: { newsItem: NewsSource1 }) => {
   return (
     <>
       <Badge variant='secondary' className='mb-4 capitalize'>
-        {newsItem.category}
+        {newsItem?.category}
       </Badge>
 
       <h2 className='font-josefin_Sans font-semibold text-lg sm:text-xl line-clamp-2'>
-        {newsItem.title}
+        {newsItem?.title}
       </h2>
 
       <p
         dangerouslySetInnerHTML={{
-          __html: purifyHTMLString(newsItem.description),
+          __html: purifyHTMLString(newsItem?.description),
         }}
         className='mt-1 font-jost font-light line-clamp-2 text-sm sm:text-base'
       ></p>
