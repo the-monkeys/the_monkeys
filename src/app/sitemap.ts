b55d69d3a5a7f107de37cef5ee31c88317b8ca61
rahullokare <lokarerahul55@gmail.com>
2024-12-14 13:51:25 +0530

    dynamic sitemap added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,33 @@
 import { baseUrl } from '@/constants/baseUrl';
+import useGetLatest100Blogs from '@/hooks/blog/useGetLatest100Blogs';
 
 export default async function sitemap() {
-  return [
+  const { blogs } = useGetLatest100Blogs();
+
+  const staticUrls = [
     {
       url: `${baseUrl}`,
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/news`,
-      changeFrequency: 'weekly',
+      url: `${baseUrl}/feed`,
+      changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: `${baseUrl}/news`,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
   ];
+
+  const blogUrls =
+    blogs?.the_blogs?.map((post) => ({
+      url: `${baseUrl}/blog/${post.blog_id}`,
+      changeFrequency: 'weekly',
+      priority: 1,
+    })) || [];
+
+  return [...staticUrls, ...blogUrls];
 }
