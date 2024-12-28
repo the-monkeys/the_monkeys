6363121ef665953b3ac2a27d541546819b75f94e
TanishqSingla <tanishqsingla00@gmail.com>
2024-12-28 21:10:52 +0530

    refactor: use pageHeader component

--- src/app/auth/create-user/page.tsx
+++ src/app/auth/create-user/page.tsx
@@ -1,16 +1,19 @@
 import Container from '@/components/layout/Container';
+import {
+  PageHeader,
+  PageHeading,
+  PageSubheading,
+} from '@/components/layout/pageHeading';
 
 import CreateUser from '../components/CreateUserForm';
 
 export default function Create() {
   return (
     <>
-      <div className='space-y-4 mt-8'>
-        <h2 className='font-arvo text-3xl text-center'>Join Monkeys</h2>
-        <p className='font-dm_sans text-sm sm:text-base opacity-80 text-center'>
-          Select an option to log in
-        </p>
-      </div>
+      <PageHeader>
+        <PageHeading heading='Join Monkeys' />
+        <PageSubheading subheading='Create your Monkeys account' />
+      </PageHeader>
       <Container className='max-w-screen-sm my-8 px-3 flex flex-col gap-8'>
         <CreateUser />
       </Container>
