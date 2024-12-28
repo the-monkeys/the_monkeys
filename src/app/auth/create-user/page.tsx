d854135ed1e911b6bd5828bfd74c735027285a7b
TanishqSingla <tanishqsingla00@gmail.com>
2024-12-28 21:15:55 +0530

    fix: add link to login on create form

--- src/app/auth/create-user/page.tsx
+++ src/app/auth/create-user/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Container from '@/components/layout/Container';
 import {
   PageHeader,
@@ -10,12 +12,21 @@ import CreateUser from '../components/CreateUserForm';
 export default function Create() {
   return (
     <>
-      <PageHeader>
+      <PageHeader className='mt-8'>
         <PageHeading heading='Join Monkeys' />
         <PageSubheading subheading='Create your Monkeys account' />
       </PageHeader>
       <Container className='max-w-screen-sm my-8 px-3 flex flex-col gap-8'>
         <CreateUser />
+        <div className='mt-4 text-center'>
+          <span className='font-dm_sans'>Already have an account? </span>
+          <Link
+            href='/auth/login'
+            className='font-dm_sans hover:underline opacity-80 text-brand-orange'
+          >
+            Login instead
+          </Link>
+        </div>
       </Container>
     </>
   );
