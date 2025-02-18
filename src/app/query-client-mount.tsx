020b03d33d8a3c4324377f2127e6f75726b1af60
TanishqSingla <tanishqsingla00@gmail.com>
2025-02-18 18:16:57 +0530

    fix: query client invalidation

--- src/app/query-client-mount.tsx
+++ src/app/query-client-mount.tsx
@@ -3,9 +3,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-export function QueryClientMount({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+export function QueryClientMount({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
