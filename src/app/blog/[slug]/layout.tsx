67305ab7a503ec1671292c099bbdbe998f70e9aa
rahullokare <lokarerahul55@gmail.com>
2025-03-09 06:04:42 +0530

    Sanitised meta title and description

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -4,18 +4,18 @@ import Container from '@/components/layout/Container';
 import { baseUrl } from '@/constants/baseUrl';
 import axiosInstanceV2 from '@/services/api/axiosInstanceV2';
 import { Blog } from '@/services/blog/blogTypes';
+import DOMPurify from 'isomorphic-dompurify';
 
 type Props = {
-  params: { slug: string }; // Fixed the type to remove unnecessary `Promise`
+  params: { slug: string };
 };
 
 export async function generateMetadata(
   { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const id = params.slug; // Extract slug directly
+  const id = params.slug;
 
-  // Fetch blog data
   let blog: Blog | null = null;
   try {
     const response = await axiosInstanceV2.get<Blog>(
@@ -26,7 +26,6 @@ export async function generateMetadata(
     console.error('Error fetching blog data:', error);
   }
 
-  // Extract OpenGraph image
   const blocks = blog?.blog?.blocks || [];
   const imageBlock = blocks.find((block) => block.type === 'image');
   const imageUrl = imageBlock?.data?.file?.url;
@@ -34,7 +33,6 @@ export async function generateMetadata(
   const fullDescription =
     descriptionBlock?.data?.text || 'No description available.';
 
-  // Truncate description to a maximum of 160 characters
   const truncateDescription = (text: string, maxLength: number): string => {
     if (text.length <= maxLength) return text;
     return text.slice(0, maxLength).trim() + '...';
@@ -42,23 +40,27 @@ export async function generateMetadata(
 
   const metaDescription = truncateDescription(fullDescription, 157);
   const parentMetadata = await parent;
-  // Construct the canonical URL
 
   const canonicalUrl = `${baseUrl}/blog/${id}`;
 
+  // Sanitize title and description using `isomorphic-dompurify`
+  const rawTitle = blocks[0]?.data?.text || parentMetadata.title;
+  const sanitizedTitle = DOMPurify.sanitize(rawTitle);
+  const sanitizedDescription = DOMPurify.sanitize(metaDescription);
+
   return {
-    title: blocks[0]?.data?.text || parentMetadata.title,
-    description: metaDescription,
+    title: sanitizedTitle,
+    description: sanitizedDescription,
     openGraph: {
-      title: blocks[0]?.data?.text || parentMetadata.description,
-      description: metaDescription,
+      title: sanitizedTitle,
+      description: sanitizedDescription,
       images: imageUrl,
       url: canonicalUrl,
     },
     twitter: {
-      title: blocks[0]?.data?.text || 'Monkeys Blog',
+      title: sanitizedTitle || 'Monkeys Blog',
       card: 'summary_large_image',
-      description: metaDescription,
+      description: sanitizedDescription,
       images: imageUrl,
     },
     alternates: {
