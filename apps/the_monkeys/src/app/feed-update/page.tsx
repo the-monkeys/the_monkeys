682b5c933ab4543480c7c52d6268bbb5df2c3bc1
piyusharmap <piyusharmap@gmail.com>
2025-06-09 21:34:40 +0530

    new feed page guidelines

---
+++ apps/the_monkeys/src/app/feed-update/page.tsx
@@ -0,0 +1,36 @@
+/* 
+Todo:
+1. Remove the search option, will place it on header
+2. Top Section
+    a. This will contain the top posts in last 24hrs
+    b. Trending topics across categoriesi, for now will do trending categories based on topics count
+        ba. for reference take a look at the top section of `https://wallhaven.cc/`
+    c. For now will showcase 6 latest posts here
+3. For-You Section
+    a. This will contain the recommended blogs
+    b. Posts from people you follow
+    c. Will be having a separate page for this section at /feed/for-you
+4. Category Section (6 posts per section)
+    a. In this section we will showcase the latest blogs from certain categories
+    b. Sports, Entertainment, Technology, Travel, Business, AI
+5. Not sure but will place a separate section for AI somewhere on the feed
+    a. Section dedicated to AI related content
+*/
+
+const FeedUpdate = () => {
+  return (
+    <div>
+      <h1>New Feed Page</h1>
+
+      {/* Create a grid system to arrange top section posts here */}
+
+      {/* Trending categories section*/}
+
+      {/* for-you section*/}
+
+      {/* category-section */}
+    </div>
+  );
+};
+
+export default FeedUpdate;
