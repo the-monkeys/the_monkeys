56c018d2c49f1438f576fc458f95f1219abef464
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2025-06-20 23:10:11 +0530

    update: moved the components to feed-update route

---
+++ apps/the_monkeys/src/app/feed-update/layout.tsx
@@ -0,0 +1,20 @@
+import Container from '@/components/layout/Container';
+import { SearchInput } from '@/components/search/SearchInput';
+
+const BlogFeedPageLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <Container className='gap-6 lg:gap-8 p-4 pb-12 space-y-6'>
+      {/* <div>
+        <SearchInput className='flex-1 mx-auto w-full sm:max-w-[500px]' />
+      </div> */}
+
+      <div className='min-h-screen'>{children}</div>
+    </Container>
+  );
+};
+
+export default BlogFeedPageLayout;
--- apps/the_monkeys/src/app/feed-update/page.tsx
+++ apps/the_monkeys/src/app/feed-update/page.tsx
@@ -1,36 +1,41 @@
-/* 
-Todo:
-1. Remove the search option, will place it on header
-2. Top Section
-    a. This will contain the top posts in last 24hrs
-    b. Trending topics across categoriesi, for now will do trending categories based on topics count
-        ba. for reference take a look at the top section of `https://wallhaven.cc/`
-    c. For now will showcase 6 latest posts here
-3. For-You Section
-    a. This will contain the recommended blogs
-    b. Posts from people you follow
-    c. Will be having a separate page for this section at /feed/for-you
-4. Category Section (6 posts per section)
-    a. In this section we will showcase the latest blogs from certain categories
-    b. Sports, Entertainment, Technology, Travel, Business, AI
-5. Not sure but will place a separate section for AI somewhere on the feed
-    a. Section dedicated to AI related content
-*/
+import { Metadata } from 'next';
 
-const FeedUpdate = () => {
+import HeroGrid from './components/hero/grid';
+import TopicFeedComponent from './components/topicFeed/topicFeed';
+import TrendingCategories from './components/trending/trending';
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: { source: string };
+}): Promise<Metadata> {
+  const title = 'Latest Blog Feed';
+  const description =
+    'Stay in the loop with the latest blogs and news on business, sports, politics, technology from around the world on Monkeys.';
+
+  return {
+    title: title,
+    description: description,
+    alternates: {
+      canonical: '/feed',
+    },
+  };
+}
+
+const BlogFeedPage = ({
+  searchParams,
+}: {
+  searchParams: { source: string };
+}) => {
+  const feedSource = searchParams.source || 'feed';
+
   return (
     <div>
-      <h1>New Feed Page</h1>
-
-      {/* Create a grid system to arrange top section posts here */}
-
-      {/* Trending categories section*/}
-
-      {/* for-you section*/}
-
-      {/* category-section */}
+      <HeroGrid />
+      <TrendingCategories />
+      <TopicFeedComponent />
     </div>
   );
 };
 
-export default FeedUpdate;
+export default BlogFeedPage;
