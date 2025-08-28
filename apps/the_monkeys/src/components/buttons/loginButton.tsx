1c6a029b71d392903ac4ccf956bf1975f0884b18
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2025-08-28 11:18:21 +0530

    Add TopicListDialog component and update login button text (#387)
    * Add TopicListDialog component and integrate it into TopicLinksContainerCompact
    
    * Update login button text from 'Join Us' to 'Log in'

--- apps/the_monkeys/src/components/buttons/loginButton.tsx
+++ apps/the_monkeys/src/components/buttons/loginButton.tsx
@@ -9,7 +9,7 @@ const LoginButton = () => {
       title='Login to Monkeys'
       className='h-9 flex items-center px-4 py-1 text-text-dark dark:text-text-light bg-background-dark dark:bg-background-light rounded-full hover:bg-background-dark/80 dark:hover:bg-background-light/80'
     >
-      <p className='font-dm_sans font-medium'>Join Us</p>
+      <p className='font-dm_sans font-medium'>Log in</p>
     </Link>
   );
 };
