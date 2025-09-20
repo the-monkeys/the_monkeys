2feb14460f952e54f7accc5053b900d06a97ca1e
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2025-09-20 09:31:24 +0530

    Person Schema added (#428)
    * Person Schema added
    
    * Main Layout SEO changed
    
    * Feed SEO changed

--- apps/the_monkeys/src/app/feed/layout.tsx
+++ apps/the_monkeys/src/app/feed/layout.tsx
@@ -1,9 +1,42 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Catch What’s Trending',
+  title:
+    'Catch What’s Trending - Latest Technology, Business, Sports & Entertainment News',
   description:
-    'Your go-to source for the latest in technology, business, sports, and entertainment — worldwide, on Monkeys.',
+    'Stay updated with the latest and most comprehensive news in technology, business, sports, and entertainment worldwide. Your go-to source for trending stories, expert insights, and breaking updates on Monkeys.',
+  keywords: [
+    'technology news',
+    'business news',
+    'sports updates',
+    'entertainment news',
+    'trending stories',
+    'latest news',
+    'breaking news',
+    'global news',
+  ],
+  openGraph: {
+    title:
+      'Catch What’s Trending - Latest Technology, Business, Sports & Entertainment News',
+    description:
+      'Stay updated with the latest and most comprehensive news in technology, business, sports, and entertainment worldwide. Your go-to source for trending stories, expert insights, and breaking updates on Monkeys.',
+    url: 'https://monkeys.com.co/feed',
+    siteName: 'Monkeys',
+    type: 'website',
+  },
+  alternates: {
+    canonical: 'https://monkeys.com.co/feed',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
+  },
 };
 
 const BlogFeedPageLayout = ({
@@ -11,7 +44,15 @@ const BlogFeedPageLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className='min-h-[800px]'>{children}</div>;
+  return (
+    <div className='min-h-[800px]'>
+      <h1 className='text-2xl font-bold hidden'>
+        Monkeys Blog Feed - Latest Technology, Business, Sports & Entertainment
+        News
+      </h1>
+      {children}
+    </div>
+  );
 };
 
 export default BlogFeedPageLayout;
