5a61aec9b021ad619de119162dbe9e26d1b18739
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2025-10-15 22:12:06 +0530

    Topics seo optimisation (#460)
    * Topic page proper SEO added with Schema
    
    * Topics sitemap added

--- apps/the_monkeys/src/app/robots.ts
+++ apps/the_monkeys/src/app/robots.ts
@@ -4,13 +4,11 @@ import { baseUrl } from '@/constants/baseUrl';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/privacy', '/cookies', '/terms'],
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/', '/auth/'],
+    },
+    sitemap: [`${baseUrl}/sitemap.xml`, `${baseUrl}/topics/sitemap.xml`],
   };
 }
