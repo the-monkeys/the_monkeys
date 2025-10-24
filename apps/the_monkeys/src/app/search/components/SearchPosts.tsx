ab545cd46a208046c392ec3f70ee5f149d39e210
Atul Kumar <singhatul0307@gmail.com>
2025-10-24 18:24:10 +0530

    Issue#450: Redirecting user to `/search?query=<inputText>` (#452)
    Added pagination and other improvements in search page

--- apps/the_monkeys/src/app/search/components/SearchPosts.tsx
+++ apps/the_monkeys/src/app/search/components/SearchPosts.tsx
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
+
+import {
+  PaginationNextButton,
+  PaginationPrevButton,
+} from '@/components/buttons/paginationButton';
 import { FeedBlogCard } from '@/components/cards/blog/FeedBlogCard';
 import { FeedBlogCardListSkeleton } from '@/components/skeletons/blogSkeleton';
+import { SEARCH_POSTS_PER_PAGE } from '@/constants/posts';
 import { useGetSearchBlog } from '@/hooks/blog/useGetSearchBlog';
 
 export const SearchPosts = ({ query }: { query: string }) => {
+  const [page, setPage] = useState<number>(0);
+  const [currentQuery, setCurrentQuery] = useState<string>(query);
+
+  if (currentQuery !== query) {
+    setCurrentQuery(query);
+    setPage(0);
+  }
+
+  const offset = page * SEARCH_POSTS_PER_PAGE;
+
   const { searchBlogs, searchBlogsLoading, searchBlogsError } =
-    useGetSearchBlog(query.trim() ? query : undefined);
+    useGetSearchBlog({
+      searchQuery: currentQuery.trim(),
+      limit: SEARCH_POSTS_PER_PAGE,
+      offset,
+    });
 
   if (searchBlogsLoading) {
     return <FeedBlogCardListSkeleton />;
@@ -20,6 +41,15 @@ export const SearchPosts = ({ query }: { query: string }) => {
 
   const blogs = searchBlogs?.blogs;
 
+  const hasNextPage =
+    blogs &&
+    searchBlogs?.total_blogs &&
+    searchBlogs?.total_blogs > (page + 1) * SEARCH_POSTS_PER_PAGE;
+
+  const hasPrevPage = page > 0;
+  const showPagination =
+    (searchBlogs?.total_blogs ?? 0) > SEARCH_POSTS_PER_PAGE;
+
   return (
     <>
       {!blogs || blogs === null ? (
@@ -28,11 +58,29 @@ export const SearchPosts = ({ query }: { query: string }) => {
         </p>
       ) : (
         <div className='max-w-4xl'>
-          <div className='flex flex-col gap-4'>
-            {blogs.slice(0, 8).map((blog) => {
+          <div className='flex flex-col gap-4 pb-4'>
+            {blogs.map((blog) => {
               return <FeedBlogCard blog={blog} key={blog?.blog_id} />;
             })}
           </div>
+
+          {showPagination && (
+            <div className='flex justify-center gap-[10px] mt-4'>
+              {hasPrevPage && (
+                <PaginationPrevButton
+                  onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+                  disable={!hasPrevPage}
+                />
+              )}
+
+              {hasNextPage && (
+                <PaginationNextButton
+                  onClick={() => setPage((prev) => prev + 1)}
+                  disable={!hasNextPage}
+                />
+              )}
+            </div>
+          )}
         </div>
       )}
     </>
