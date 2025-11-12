72f83f95945de45a81adeef7f6f46c2232d4511b
TanishqSingla <tanishqsingla00@gmail.com>
2025-11-12 23:18:48 +0530

    fix: login from blog redirects to home page

--- apps/the_monkeys/src/app/blog/components/BlogReactions.tsx
+++ apps/the_monkeys/src/app/blog/components/BlogReactions.tsx
@@ -22,7 +22,7 @@ export const BlogReactions = ({
     return (
       <div className='p-1 flex justify-center items-center gap-1'>
         <Link
-          href={LOGIN_ROUTE}
+          href={`${LOGIN_ROUTE}?callbackURL=${globalThis.location}`}
           className='text-sm font-medium text-brand-orange underline'
         >
           Login
