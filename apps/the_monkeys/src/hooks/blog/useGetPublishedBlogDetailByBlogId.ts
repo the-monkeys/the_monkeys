70a256dbc9c2fc73ea6a6940893ed60136f7e8bf
Rahul Lokare <lokarerahul55@gmail.com>
2025-12-14 09:26:40 +0530

    csr to ssr

--- apps/the_monkeys/src/hooks/blog/useGetPublishedBlogDetailByBlogId.ts
+++ apps/the_monkeys/src/hooks/blog/useGetPublishedBlogDetailByBlogId.ts
@@ -1,20 +1,20 @@
 import { Blog } from '@/services/blog/blogTypes';
 import { authFetcherV2 } from '@/services/fetcher';
-import useSWR from 'swr';
+import { useQuery } from '@tanstack/react-query';
+
+export const BLOG_DETAIL_QUERY_KEY = 'blog-detail';
 
 const useGetPublishedBlogDetailByBlogId = (blogId: string | undefined) => {
-  const { data, error, isLoading } = useSWR<Blog>(
-    `/blog/${blogId}`,
-    authFetcherV2,
-    {
-      revalidateOnFocus: false,
-      refreshInterval: 0,
-    }
-  );
+  const { data, error, isLoading, isError } = useQuery<Blog, Error>({
+    queryKey: [BLOG_DETAIL_QUERY_KEY, blogId],
+    queryFn: () => authFetcherV2(`/blog/${blogId}`),
+    enabled: !!blogId,
+    staleTime: 60 * 1000,
+  });
 
   return {
     blog: data,
-    isError: error,
+    isError: isError || !!error,
     isLoading,
   };
 };
