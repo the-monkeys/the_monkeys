6df988abbfec24be78e31cce3f6797f22ecfdc05
Dave Augustus <devpandey19924u@gmail.com>
2026-01-05 21:25:54 +0530

    chore: add comment to utils

--- apps/the_monkeys/src/lib/utils.ts
+++ apps/the_monkeys/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+// Utility function to merge tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
