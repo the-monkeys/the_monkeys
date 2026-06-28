fd7d708443603b3d508f35ce41ea4fc659174ef8
surajgoraicse <surajgoraicse@gmail.com>
2026-06-28 09:00:57 +0530

    refactor: replace button with Link component for navigation and prefetching

--- apps/the_monkeys/src/components/buttons/createButton.tsx
+++ apps/the_monkeys/src/components/buttons/createButton.tsx
@@ -1,28 +1,21 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { CREATE_ROUTE } from '@/constants/routeConstants';
 
 import Icon from '../icon';
 
 export const CreateButton = () => {
-  const router = useRouter();
-
-  const handleCreate = () => {
-    router.push(CREATE_ROUTE);
-  };
-
   return (
-    <button
-      onClick={handleCreate}
-      // Prefetch the edit route on hover to speed up the subsequent navigation
-      onMouseEnter={() => router.prefetch('/edit/[blogId]')}
+    <Link
+      href={CREATE_ROUTE}
+      prefetch
       title='Create Post'
       className='group h-9 flex items-center gap-1 px-[6px] sm:px-4 py-[6px] border-2 border-brand-orange text-white bg-brand-orange rounded-full hover:bg-brand-orange/20 hover:text-brand-orange transition-all active:scale-95'
     >
       <Icon name='RiAdd' />
       <p className='hidden sm:block font-dm_sans font-bold'>Create</p>
-    </button>
+    </Link>
   );
 };
