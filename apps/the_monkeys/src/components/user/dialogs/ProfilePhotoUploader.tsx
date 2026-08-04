b5eb718b6c1b83b561affdf00234dad091298360
Ritu Raj <riturajgudakesh@gmail.com>
2026-08-05 03:04:56 +0530

    fixed bot issues

--- apps/the_monkeys/src/components/user/dialogs/ProfilePhotoUploader.tsx
+++ apps/the_monkeys/src/components/user/dialogs/ProfilePhotoUploader.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import Image from 'next/image';
 
